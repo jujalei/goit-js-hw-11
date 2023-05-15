e414d6edf0c4e2d62864b5df38da2c17a06f24b0
YuliyaYaremchuk <118767028+jujalei@users.noreply.github.com>
2023-05-15 22:01:02 +0300

    add personal key

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { refs } from './js/refs';
---
+++ src/js/pixabay.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+const KEY = 'key=36427900-70ae1c90a640d0a3e2de3b856';
